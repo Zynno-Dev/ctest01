83143b43b219ea82ca28f4812caff4eed72a2e8c
agustinalmiron32 <agustinalmiron32@gmail.com>
2021-05-04 11:53:01 -0300

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using static WebApplication1.Models.DatosClassModel;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Random()
+        {
+            return "Este es el segundo test";
+        }
+
+/*        public string Chizzi(DatosClassModel.Edad,Nombre,Apellido,DNI)
+        {
+            return HtmlEncoder.Default.Encode($"Nombre: {Nombre}, Apellido: {Apellido}, Edad: {Edad}, Dni: {DNI}");
+        } */
+    }
+}
---
+++ Models/DatosClassModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DatosClassModel
+    {
+        string Nombre = "Carlos";
+        string Apellido = "Michelin";
+        int Edad = 52;
+        string DNI = "20.345.213";
+
+    }
+}
